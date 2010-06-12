e3e83573297d0bd56753a243971b116fe220505d
unknown <Jacob@.(none)>
2010-06-12 12:35:16 -0700

    Now searches for test directories.

--- src/compileResults4.py
+++ src/compileResults4.py
@@ -9,25 +9,31 @@ from math import *
 
 globalCount = 0
 
-for i in range(0,16):
-	for j in range(0,4):
-		dist = 0.2*j + 0.5
+dirs = []
+files = os.listdir(".")
+for nameF in files:
+	if "results" == nameF[0:7]:
+		dirs.append(nameF)
 		
-		dirName = "./" + "results%02u_%f" % (i, dist)
-		#print "./" + "results%02u_%f" % (i, dist)
-		files = os.listdir(dirName)
-		
-		scenes = []
-		for nameF in files:
-			if "scene" == nameF[0:5]:
-				scenes.append(nameF)	
+dirs.sort()
 
-		scenes.sort()
+for dirName in dirs:
 
-		for scene in scenes:
-			print "cp " + dirName + "/" + scene + " scene%06u.png" % globalCount
-			#os.system("cp " + dirName + "/" + scene + " scene%06u.png" % globalCount)
-			globalCount += 1
+	#dirName = "./" + "results%02u_%f" % (i, dist)
+	#print "./" + "results%02u_%f" % (i, dist)
+	files = os.listdir(dirName)
+	
+	scenes = []
+	for nameF in files:
+		if "scene" == nameF[0:5]:
+			scenes.append(nameF)	
+
+	scenes.sort()
+
+	for scene in scenes:
+		#print "cp " + dirName + "/" + scene + " scene%06u.png" % globalCount
+		os.system("cp " + dirName + "/" + scene + " scene%06u.png" % globalCount)
+		globalCount += 1
 
 #exit()
 
