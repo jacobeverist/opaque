e4fa9317d9ff1052ac1a078893cdf25fe9e63636
unknown <everist@.(none)>
2013-07-23 00:36:43 -0700

    Added OpenCV binaries just for backward-compatibility.  Changed bulletprobe compile script based on out-of-the-box experience.

--- src/modules/bulletprobe/setup.py
+++ src/modules/bulletprobe/setup.py
@@ -14,12 +14,13 @@ ext_modules = [Extension("bulletsnakeprobe",
 		["bulletsnakeprobe.pyx", "BulletSnake.cpp"],
 		language="c++",
 		include_dirs=[BULLET_DIR + '\\src'],
+		libraries=["BulletDynamics_Debug", "LinearMath_Debug", "BulletCollision_Debug"],
 		#libraries=["BulletDynamics", "linearmath", "BulletCollision"],
-		libraries=["BulletDynamics_vs2008_debug", "LinearMath_vs2008_debug", "BulletCollision_vs2008_debug"],
+		#libraries=["BulletDynamics_vs2008_debug", "LinearMath_vs2008_debug", "BulletCollision_vs2008_debug"],
 		#libraries=["BulletDynamics_vs2008", "LinearMath_vs2008", "BulletCollision_vs2008"],
-		library_dirs=[BULLET_DIR + '\\lib'],
+		#library_dirs=[BULLET_DIR + '\\lib'],
 		#library_dirs=[BULLET_DIR + '\\lib\\Release'],
-		#library_dirs=[BULLET_DIR + '\\lib\\Debug'],
+		library_dirs=[BULLET_DIR + '\\lib\\Debug'],
 								)]
 
 setup(
