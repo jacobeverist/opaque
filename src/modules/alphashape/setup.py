c12c6f8bfff87d1e1934e558447254a4f3902ea2
unknown <everist@.(none)>
2013-07-23 16:19:45 -0700

    alpha shape setup, dependency walker tool

---
+++ src/modules/alphashape/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [Extension("alphamod", ["alphamod.pyx", "alpha2.cpp"],
+			pyrex_gdb=True,
+                        language="c++",
+			extra_compile_args=['/EHsc'],
+			include_dirs=['CGAL-4.1/include', 'boost_1_53_0', 'CGAL-4.1/auxiliary/gmp/include'],
+			library_dirs=['CGAL-4.1/lib', 'boost_1_53_0/stage/lib']
+			)]
+#extra_link_args=['-framework', 'CoreFoundation']
+
+#ext_modules = [Extension("toromod", ["toromod.pyx", "toro.cpp", "posegraph2.cpp", "treeoptimizer2.cpp", "toro.cpp"], language="c++")]
+#ext_modules = [Extension("transform", ["transform.pyx"]), Extension("stability", ["stability.pyx"], language="c++")]
+
+setup(
+  name = 'Alpha Shape 2D',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules
+)
+
