14aac564e2977897fcbaf3ca73246bc7d30ef47f
Jacob Everist <everist@snapstation.(none)>
2014-05-01 08:02:32 -0700

    added spatialFeatures member to PoseData

--- src/opaque/maps/BayesMapper.py
+++ src/opaque/maps/BayesMapper.py
@@ -77,6 +77,7 @@ class BayesMapper:
 		self.poseData.isNodeFeatureless = {}
 		self.poseData.frontProbeError = {}
 		self.poseData.backProbeError = {}
+		self.poseData.spatialFeatures = {}
 
 	
 		self.pathDrawCount = 0
@@ -152,6 +153,7 @@ class BayesMapper:
 		self.poseData.frontProbeError[nodeID] = newNode.frontProbeError
 		self.poseData.backProbeError[nodeID] = newNode.backProbeError
 		self.poseData.travelDirs[nodeID] = newNode.travelDir
+		self.poseData.spatialFeatures[nodeID] = newNode.spatialFeatures
 
 
 		for mid, mapHyp in self.mapHyps.iteritems():
@@ -193,6 +195,8 @@ class BayesMapper:
 		existMapHyp = self.mapHyps
 		existParticleIDs = self.particleIDs
 		existWalls = self.walls
+
+		self.travelDirs
 
 		exec(saveStr)
 		self.poseData.travelDirs = self.poseData.travelDirs[nodeID]
@@ -445,6 +449,7 @@ class BayesMapper:
 		saveFile += "self.poseData.frontProbeError = " + repr(self.poseData.frontProbeError) + "\n"
 		saveFile += "self.poseData.backProbeError = " + repr(self.poseData.backProbeError) + "\n"
 		saveFile += "self.poseData.travelDirs = " + repr(self.poseData.travelDirs) + "\n"
+		saveFile += "self.poseData.spatialFeatures = " + repr(self.poseData.spatialFeatures) + "\n"
 
 		saveFile += "mapHypIDs = " + repr(self.mapHyps.keys()) + "\n"
 
