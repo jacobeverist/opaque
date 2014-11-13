9207421ab2f004f7f95c41f6bcbb68b0e48b6668
Jacob Everist <everist@snapstation.(none)>
2014-11-13 13:45:06 -0800

    Docker container build for dark mapper

---
+++ ciba/docker/build_images.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+WORKING_COPY_PATH=/home/everist/opaque
+
+fileName=opaque_0.1
+
+rm opaque_0.1.tar
+rm -rf opaque_0.1
+
+git clone $WORKING_COPY_PATH opaque_0.1
+tar cf opaque_0.1.tar opaque_0.1
+
+#rm -rf output_centos_dark_mapper
+
+#packer/packer build scripts/kvm.json
+
+
+
+
+exit
+
+qemu-img convert -O vmdk -o subformat=monolithicSparse output_centos_dark_mapper/$fileName.raw output_centos_dark_mapper/$fileName.vmdk.tmp
+
+vboxmanage clonehd --format=VMDK --variant=Stream output_centos_dark_mapper/$fileName.vmdk.tmp output_centos_dark_mapper/$fileName.vmdk
+ 
+rm output_centos_dark_mapper/$fileName.vmdk.tmp
+
+# BYTE_SIZE = bytes of vmdk image
+# VMDK_NAME = name of vmdk image: cloud_barometer_rc1.vmdk
+# VIRT_NAME = name of virtual machine: "ctbs-base-ovf"
+# VIRT_ID = ID of virtual machine: "ctbs-base-ovf"
+
+
+rm -rf output_centos_dark_mapper/$fileName\_ovf
+mkdir output_centos_dark_mapper/$fileName\_ovf
+
+mv output_centos_dark_mapper/$fileName.vmdk output_centos_dark_mapper/$fileName\_ovf/$fileName.vmdk
+#cp output_centos_dark_mapper/$fileName.vmdk output_centos_dark_mapper/$fileName\_ovf/$fileName.vmdk
+
+#capSize="10485760000"
+capSize=`ls -l output_centos_dark_mapper/$fileName.raw | awk '{print $5}'`
+fileSize=`ls -l output_centos_dark_mapper/$fileName\_ovf/$fileName.vmdk | awk '{print $5}'`
+
+sed s/\$BYTE_SIZE/$fileSize/ scripts/ctbs-base-ovf.ovf > output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf
+sed -i".bak" s/\$VMDK_NAME/$fileName.vmdk/ output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf
+sed -i".bak" s/\$VIRT_NAME/$fileName/ output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf
+sed -i".bak" s/\$VIRT_ID/$fileName/ output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf
+sed -i".bak" s/\$CAP_SIZE/$capSize/ output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf
+rm output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf.bak
+
+
+# create manifest file
+vmdkName="SHA1($fileName.vmdk) ="
+vmdkSum=`sha1sum output_centos_dark_mapper/$fileName\_ovf/$fileName.vmdk | awk '{print $1}'`
+vmdkCheck="$vmdkName $vmdkSum"
+echo $vmdkCheck > output_centos_dark_mapper/$fileName\_ovf/$fileName.mf
+
+ovfName="SHA1($fileName.ovf) ="
+ovfSum=`sha1sum output_centos_dark_mapper/$fileName\_ovf/$fileName.ovf | awk '{print $1}'`
+ovfCheck="$ovfName $ovfSum"
+echo $ovfCheck >> output_centos_dark_mapper/$fileName\_ovf/$fileName.mf
+
+#exit
+
+rm -rf release-v2.0.0
+svn export $WORKING_COPY_PATH/release/v2.0.0 release-v2.0.0
+rm -rf release-v2.0.0/labels
+
+cp cloud_barometer_2.0.0.tar.bz2 release-v2.0.0/sw-disk1
+md5sum cloud_barometer_2.0.0.tar.bz2 > release-v2.0.0/sw-disk1/cloud_src_checksum
+
+cp output_centos_dark_mapper/$fileName.raw release-v2.0.0/kvm-disk2/
+cd release-v2.0.0/kvm-disk2
+#tar cvf kvm.tar $fileName.raw
+bzip2 $fileName.raw
+md5sum $fileName.raw.bz2 > cloud_raw_checksum
+#mv kvm.tar.bz2 release-v2.0.0/kvm-disk2
+
+cd ../..
+
+cp -R output_centos_dark_mapper/$fileName\_ovf release-v2.0.0/vmware-disk3/
+cd release-v2.0.0/vmware-disk3
+tar cf $fileName\_ovf.tar $fileName\_ovf
+#bzip2 $fileName\_ovf.tar
+#md5sum $fileName\_ovf.tar.bz2 > cloud_ovf_checksum
+md5sum $fileName\_ovf.tar > cloud_ovf_checksum
+#mv vmware.tar.bz2 release-v2.0.0/vmware-disk3
+rm -rf $fileName\_ovf
+
+cd ../..
+
+rm -rf isos
+mkdir isos
+mkisofs -JR -o isos/barometer_src_2.0.0.iso release-v2.0.0/sw-disk1/
+mkisofs -JR -o isos/barometer_kvm_2.0.0.iso release-v2.0.0/kvm-disk2/
+mkisofs -JR -o isos/barometer_ovf_2.0.0.iso release-v2.0.0/vmware-disk3/
+mkisofs -JR -o isos/barometer_all_2.0.0.iso release-v2.0.0/
+
+
--- ciba/docker/installStuff.sh
+++ ciba/docker/installStuff.sh
@@ -1,41 +1,74 @@
 #!/bin/bash
 
-easy_install argparse
-
-rpm -Uvh p7zip-9.20.1-1.el6.rf.x86_64.rpm
-
-tar zxf cmake-2.8.12.2.tar.gz
-cd cmake-2.8.12.2
-./configure
+cd /src
+tar bullet-2.77.tgz
+cd bullet-2.77
+cmake -DBUILD_SHARED_LIBS=true -DCMAKE_CXX_FLAGS=-fpermissive .
 make
 make install
-cd
+cd /src
+
+rpm -Uvh ois-1.3.0-7.fc22.x86_64.rpm 
+rpm -Uvh ois-devel-1.3.0-7.fc22.x86_64.rpm 
+
+unzip FreeImage3160.zip
+cd FreeImage
+make
+make install
+cd /src
+
+cd /src
+tar jxf ogre_src_v1-8-1.tar.bz2
+cd ogre_src_v1-8-1
+cmake .
+make
+make install
+cd /src
+
+#./autogen.sh
+#./configure
+#make
+#make install
+#cp src/BulletDynamics/ConstraintSolver/btFixedConstraint.h /usr/local/include/bullet/BulletDynamics/ConstraintSolver/
 
 
-tar zxf Cython-0.20.1.tar.gz
-cd Cython-0.20.1
-python setup.py build
-python setup.py install
-cd 
 
-tar zxf numpy-1.7.2.tar.gz
-cd numpy-1.7.2
-python setup.py build
-python setup.py install
-cd
+easy_install argparse
 
-tar zxf scipy-0.13.3.tar.gz
-cd scipy-0.13.3
-python setup.py build
-python setup.py install
-cd
+#rpm -Uvh p7zip-9.20.1-1.el6.rf.x86_64.rpm
+
+#tar zxf cmake-2.8.12.2.tar.gz
+#cd cmake-2.8.12.2
+#./configure
+#make
+#make install
+#cd
 
 
-tar zxf matplotlib-1.3.1.tar.gz
-cd matplotlib-1.3.1
-python setup.py build
-python setup.py install
-cd
+#tar zxf Cython-0.20.1.tar.gz
+#cd Cython-0.20.1
+#python setup.py build
+#python setup.py install
+#cd 
+
+#tar zxf numpy-1.7.2.tar.gz
+#cd numpy-1.7.2
+#python setup.py build
+#python setup.py install
+#cd
+
+#tar zxf scipy-0.13.3.tar.gz
+#cd scipy-0.13.3
+#python setup.py build
+#python setup.py install
+#cd
+
+
+#tar zxf matplotlib-1.3.1.tar.gz
+#cd matplotlib-1.3.1
+#python setup.py build
+###python setup.py install
+#cd
 
 tar zxf pca_module-1.1.02.tar.gz
 cd pca_module-1.1.02
@@ -45,21 +78,22 @@ sed -i".bak" 's/old_numeric = True/old_numeric = False/' setup.py
 rm setup.py.bak
 python setup.py build
 python setup.py install
-cd
+cd /src
 
 unzip python-graph-1.3.1.zip
 cd python-graph
 python setup.py build
 python setup.py install
-cd
+cd /src
 
-unzip opencv-2.4.8.zip
-cd opencv-2.4.8
+unzip opencv-2.4.9.zip
+cd opencv-2.4.9
 cmake .
 make
 make install
-cd
+cd /src
 
+cd /src
 tar xvf CGAL-4.4.tar.gz
 cd CGAL-4.4
 
@@ -77,20 +111,22 @@ rm -f include/CGAL/Mpzf.h.bak
 cmake .
 make
 make install
-cd ..
+cd /src
 
 tar xvf guppy-0.1.10.tar.gz
 cd guppy-0.1.10
 python setup.py build
 python setup.py install
-cd
+cd /src
 
 
 
+mkdir opaque_0.1
+cd opaque_0.1
+tar xf ../opaque_0.1.tar 
 
-tar xf opaque_0.1.tar
 
-cd opaque_0.1/src/modules
+cd src/modules
 python setup.py build_ext --inplace
 
 cd medialaxis
@@ -103,13 +139,21 @@ cd ..
 
 cd nelmin
 python setup.py build_ext --inplace
+cd ..
+
+cd bulletprobe
+python setup.py build_ext --inplace
+cd ..
+
+cd ogre
+python setup.py build_ext --inplace
 cd ../..
 
 
-mkdir results
-cd results
-tar zxf ~/test_data.tar.gz
-cd ..
+#mkdir results
+#cd results
+#tar zxf ~/test_data.tar.gz
+#cd ..
 
 
 mkdir testDir
