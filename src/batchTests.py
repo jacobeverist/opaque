3c91255cca64cea95ec7aa610eea213131ce8f77
Jacob Everist <everist@snapstation.(none)>
2015-01-04 02:23:33 -0800

    updated core batchTest file

--- src/batchTests.py
+++ src/batchTests.py
@@ -11,24 +11,27 @@ parser = argparse.ArgumentParser(description='DarkMapper Batch')
 parser.add_argument('--startOffset', type=float, default=0.0, help="probe start displacement offset")
 parser.add_argument('--maxNumPoses', type=int, default=100, help="max number of poses")
 
-# bendFeature
-parser.add_argument('--bendFeature', dest='bendFeature', action='store_true')
-parser.set_defaults(bendFeature=False)
-
-
 args = parser.parse_args()
 
 
-easyMaps = ['60_left_junction.py', '60_right_junction.py', 'L_89_left_junction.py', 'L_89_right_junction.py']
+#maps = ['60_left_junction.py', '60_right_junction.py', 'L_left_junction.py', 'L_right_junction.py']
+hardMaps = ['Y_junction.py', 'T_side_junction.py', 'T_bottom_junction.py', 'cross_junction.py']
+#maps = ['L_left_junction.py', 'L_right_junction.py']
+maps = ['L_right_junction.py', 'T_bottom_junction.py']
 
-#hardMaps = ['cross_junction.py', 'T_side_junction.py', 'T_bottom_junction.py', 'Y_junction.py']
-hardMaps = ['cross_junction.py', 'T_side_junction.py', 'T_89_junction.py', 'Y_junction.py']
+#maps += hardMaps
+#maps = ['cross_junction.py', 'Y_junction.py', 'Y_Y_junction.py']
+#maps = ['cross_junction.py']
 
 comboMaps = ['Y_Y_junction.py', 'Y_right_Y_junction.py', 'Y_right_cross_junction.py', 'Y_left_Y_junction.py', 'T_side_T_junction.py', 'T_bottom_T_junction.py', 'L_right_left_junction.py', 'L_left_right_junction.py', '60_right_left_junction.py', '60_left_right_junction.py', 'cross_cross_junction.py', 'gen_function.py', 'gen_function2.py', 'gen_junction.py', 'gen_junction2.py']
 
-#maps = [hardMaps[0],] + easyMaps + hardMaps[1:]
-#maps = easyMaps
-maps = hardMaps
+
+#>>> args = shlex.split(command_line)
+#>>> print args
+#['/bin/vikings', '-input', 'eggs.txt', '-output', 'spam spam.txt', '-cmd', "echo '$MONEY'"]
+#>>> p = subprocess.Popen(args) # Success!
+# python startSim.py --mapfile=../mapLibrary/cross_junction.py --restoreConfig=True --hideWindow=True &> out.txt
+
 
 
 """
@@ -47,24 +50,47 @@ for mapFile in maps:
 	" run the test "
 	fileOut = open('out.txt', 'a')
 	#command_line = "/usr/bin/python ../startSim.py --mapfile=../mapLibrary/%s --restoreConfig=True --hideWindow=True" % mapFile 
-	if args.bendFeature:
-		command_line = "/usr/bin/python ../startSim.py --hideWindow=True --restoreConfig=True --mapFile=../mapLibrary/%s --maxNumPoses=%d --startOffset=%1.2f --bendFeature" % (mapFile, maxNumPoses, startOffset)
-	else:
-		command_line = "/usr/bin/python ../startSim.py --hideWindow=True --restoreConfig=True --mapFile=../mapLibrary/%s --maxNumPoses=%d --startOffset=%f" % (mapFile, maxNumPoses, startOffset)
-
+	command_line = "/usr/bin/python ../startSim.py --hideWindow=True --restoreConfig=True --mapFile=../mapLibrary/%s --maxNumPoses=%d --startOffset=%f &> out.txt" % (mapFile, maxNumPoses, startOffset)
 	
-	parsed_args = shlex.split(command_line)
-	print "args:", parsed_args
+	args = shlex.split(command_line)
+	print "args:", args
 	#p = subprocess.Popen(args, stdout=fileOut, stderr=fileOut)
 
-	subprocess.check_call(parsed_args, stdout=fileOut, stderr=fileOut)
+	subprocess.check_call(args, stdout=fileOut, stderr=fileOut)
+
+	""" monitor whether or not the execution has halted by watching out files """
+	"""
+	while True:
+		outFiles = glob.glob(os.path.join("*.out")) + glob.glob(os.path.join("out.txt"))
+		outFiles.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+
+		modTime = os.path.getmtime(outFiles[0])
+		currTime = time.time()
+
+		timeDiff = currTime - modTime
+
+		print outFiles[0], timeDiff
+
+		if timeDiff > 60.0:
+			p.kill()
+			break
+
+		time.sleep(1)
+	"""
+
+	#p.wait()
+
+
+	#os.system("python ../startSim.py --mapfile=../mapLibrary/%s --restoreConfig=True --hideWindow=True &> out.txt" % mapFile)
+	#subprocess.Popen(["python", "../startSim.py ", "--mapfile=../mapLibrary/%s" % mapFile, "--restoreConfig=True", "--hideWindow=True", "&>", "out.txt"], shell=True)
+	# &> out.txt"])
 
 	"paste results together"
 	#os.system("python compileResults3.py")
 		
 	" mkdir "
 	testDir = "results_%s" % mapFile
-	os.mkdir(testDir)
+	os.system("mkdir " + testDir)
 
 	outFiles = glob.glob(os.path.join("*.out"))
 	outFiles += glob.glob(os.path.join("*.err"))
@@ -73,8 +99,12 @@ for mapFile in maps:
 	outFiles += glob.glob(os.path.join("*.png"))
 
 
-	" move files "
 	for outFile in outFiles:
 		shutil.move(outFile, testDir)
 		
+	" move files "
+	#os.system("mv *.png *.obj *.txt *.out *.err " + testDir )
 	
+	" clean up "
+	pass
+
